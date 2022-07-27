2030af2a8f0e7965f593a112df0f2e5d2622b5b5
Antonio Cruz <antonio.cruz5612@gmail.com>
2022-07-26 20:19:14 -0700

    change 'console.log' on line 19 to 'return'.

--- solution.js
+++ solution.js
@@ -16,7 +16,7 @@ const twoSum = (nums, target) => {
     for (let i = 0; i < nums.length; i++) {
         for (let j = i+1; j < nums.length; j++) {
             if (nums[i] + nums[j] == target) {
-                console.log([i, j]);
+                return [i, j];
             }
         }
     }   
