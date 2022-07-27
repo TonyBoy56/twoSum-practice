9da2656149cdc2ba44cdab1972a2c490e1706111
Antonio Cruz <antonio.cruz5612@gmail.com>
2022-07-26 20:13:45 -0700

    correct an error on line 15 in which the value 'j' was being iterated instead of 'i'.

--- solution.js
+++ solution.js
@@ -12,7 +12,7 @@ const nums = [2, 7, 11, 15];
 const target = 9;
 
 const twoSum = (nums, target) => {
-    for (let i = 0; i < nums.length; j++) {
+    for (let i = 0; i < nums.length; i++) {
         for (let j = i+1; j < nums.length; j++) {
             if (nums[i] + nums[j] == target) {
                 console.log([i, j]);
