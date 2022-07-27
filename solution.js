d7337044f1254a34332906c2da3ca4d835a16f19
Antonio Cruz <antonio.cruz5612@gmail.com>
2022-07-26 20:06:17 -0700

    create an imbedded 'for' loop to iterate ahead of 'i', with 'j'.

--- solution.js
+++ solution.js
@@ -12,7 +12,9 @@ const nums = [2, 7, 11, 15];
 const target = 9;
 
 const twoSum = (nums, target) => {
-    for (let i=0; i < nums.length; j++) {
-
+    for (let i = 0; i < nums.length; j++) {
+        for (let j = i+1; j < nums.length; j++) {
+            
+        }
     }   
 }
