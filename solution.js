87a7606ee8005e4d90d1f8e8b0f57c35be969ee5
Antonio Cruz <antonio.cruz5612@gmail.com>
2022-07-26 20:09:44 -0700

    change the 'return' statement on line 18 to 'console.log' to view the output within the console.

--- solution.js
+++ solution.js
@@ -15,7 +15,7 @@ const twoSum = (nums, target) => {
     for (let i = 0; i < nums.length; j++) {
         for (let j = i+1; j < nums.length; j++) {
             if (nums[i] + nums[j] == target) {
-                return [i, j];
+                console.log([i, j]);
             }
         }
     }   
