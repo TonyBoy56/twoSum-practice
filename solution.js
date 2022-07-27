580ee869b6594a033a63101326fd4cc4bcc8dd8a
Antonio Cruz <antonio.cruz5612@gmail.com>
2022-07-26 20:08:26 -0700

    create a conditional 'if' statement to check that the respective indices of 'i' and 'j' add up to the target value.

--- solution.js
+++ solution.js
@@ -14,7 +14,9 @@ const target = 9;
 const twoSum = (nums, target) => {
     for (let i = 0; i < nums.length; j++) {
         for (let j = i+1; j < nums.length; j++) {
-            
+            if (nums[i] + nums[j] == target) {
+                return [i, j];
+            }
         }
     }   
 }
